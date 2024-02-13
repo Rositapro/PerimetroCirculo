4dd16b2db5bb09dc2b72eb7ef9c3b84159a3c763
abrah <abrah@HPDEABRAHAM>
2024-02-12 21:28:52 -0600

    Agregar archivos de proyecto.

---
+++ PerimetroCirculo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double d, p;
+Console.WriteLine("Enter the diameter: ");
+d = double.Parse(Console.ReadLine());
+p = 3.1416 * d;
+Console.WriteLine("The perimeter of the circle is:" + p);
+Console.ReadLine();
+Console.ReadKey();
